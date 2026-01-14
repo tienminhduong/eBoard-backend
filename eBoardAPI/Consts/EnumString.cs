e22051ef201bdbf19b5281d2b86c1124f518a36c
Tiền Minh Dương <tmd31052005@gmail.com>
2026-01-14 23:14:50 +0700

    Refactor const naming

--- eBoardAPI/Consts/EnumString.cs
+++ eBoardAPI/Consts/EnumString.cs
@@ -2,13 +2,13 @@ namespace eBoardAPI.Consts;
 
 public static class RelationshipWithParent
 {
-    public const string Father = "Cha";
-    public const string Mother = "Mẹ";
-    public const string Guardian = "Người giám hộ";
+    public const string FATHER = "Cha";
+    public const string MOTHER = "Mẹ";
+    public const string GUARDIAN = "Người giám hộ";
 }
 
 public static class Gender
 {
-    public const string Male = "Nam";
-    public const string Female = "Nữ";
+    public const string MALE = "Nam";
+    public const string FEMALE = "Nữ";
 }
