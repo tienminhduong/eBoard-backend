e2660501de102f0c076c0f261c84adaf4bc7d227
Tiền Minh Dương <tmd31052005@gmail.com>
2026-01-23 00:53:36 +0700

    Add student id in activity participant dto

--- eBoardAPI/Models/Activity/ActivityParticipantDto.cs
+++ eBoardAPI/Models/Activity/ActivityParticipantDto.cs
@@ -3,6 +3,7 @@ namespace eBoardAPI.Models.Activity;
 public class ActivityParticipantDto
 {
     public Guid Id { get; set; }
+    public Guid StudentId { get; set; }
     public string StudentName { get; set; } = string.Empty;
     public string ParentPhoneNumber { get; set; } = string.Empty;
     public string TeacherComments { get; set; } = string.Empty;
